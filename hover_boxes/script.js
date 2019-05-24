3a2bfcc89a0f29c147865cade7d116365aca0d85
Kevin Jolley <serrowpls@gmail.com>
2019-05-24 17:41:45 -0500

    added conditionals for switching between the 3 cards

--- hover_boxes/script.js
+++ hover_boxes/script.js
@@ -1,3 +1,33 @@
 $(".move-button").on("click", function() {
   console.log("I work uwu");
 });
+
+function hideCards() {
+  $("#card-1").removeClass("showme");
+  $("#card-2").removeClass("showme");
+  $("#card-3").removeClass("showme");
+}
+
+$(document).ready(function() {
+  $("#card-1").addClass("showme");
+});
+
+$("#right").on("click", function() {
+  if ($("#card-2").hasClass("showme")) {
+    hideCards();
+    $("#card-1").addClass("showme");
+  } else {
+    hideCards();
+    $("#card-2").addClass("showme");
+  }
+});
+
+$("#left").on("click", function() {
+  if ($("#card-3").hasClass("showme")) {
+    hideCards();
+    $("#card-1").addClass("showme");
+  } else {
+    hideCards();
+    $("#card-3").addClass("showme");
+  }
+});
